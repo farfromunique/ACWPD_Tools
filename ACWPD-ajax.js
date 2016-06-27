7732473695131cf92de5288dc7e6d837c9257e50
Aaron C <git@acwpd.com>
2016-06-26 21:15:02 -0700

    Adding more functions

--- ACWPD-ajax.js
+++ ACWPD-ajax.js
@@ -23,14 +23,6 @@ function adjustMyURL(displayURL) {
 	window.history.pushState({site:sRoot}, siteName, "/" + displayURL + "/");
 }
 
-document.onreadystatechange = function() { 
-	if (document.readyState === 4) { // Document is fully loaded
-		document.querySelector('.class').onclick = function() {
-			//do stuff
-		}
-	}
-}
-
 function loadMyPage(pageName, displayURL, targetDiv) {
 	var ajax;
 	document.getElementById("content").innerHTML = "Loading ...";
@@ -84,5 +76,73 @@ function DismissImportant() {
 function hideMessage() {
 	if(document.getElementById('Message')) {
 		document.getElementById('Message').style.display='none';
+	}
+}
+
+function addEventToID(selector, eventType, functionText) {
+	var commandText;
+	if (!window[selector]) { return false; };
+	commandText = "document.getElementById('" + selector;
+	eventType = eventType.toLowerCase();
+	switch (eventType) {
+		case "click":
+			commandText = commandText + "').onclick";
+			break;
+		
+		case "load":
+			commandText = commandText + "').onload";
+			break;
+			
+		case "change":
+			commandText = commandText + "').onchange";
+			break;
+		
+		case "mouseover":
+			commandText = commandText + "').onmouseover";
+			break;
+		
+		case "mouseout":
+			commandText = commandText + "').onmouseout";
+			break;
+		
+		case "submit":
+			commandText = commandText + "').onsubmit";
+			break;
+		
+		case "doubleclick":
+			commandText = commandText + "').ondblclick";
+			break;
+		
+		/* invalid input handler */
+		default:
+			alert("Invalid eventType provided. Aborting!");
+			return;
+	}
+	
+	commandText = commandText + " = function () { " + functionText + "};";
+	eval(commandText);
+}
+
+function removeAllMyEventListeners(id) {
+	id.onclick = "";
+	id.onload = "";
+	id.onchange = "";
+	id.onmouseover = "";
+	id.onmouseout = "";
+	id.onsubmit = "";
+	id.ondblclick = "";
+}
+
+function remove(id) {
+	try {
+		id.remove()
+	} 
+	catch (e) {
+		try {
+			id.removeNode()
+		}
+		catch (f) {
+			error.log ("Tried object.remove: " + e + "\n Then tried object.removeNode: " + f)
+		}
 	}
 }

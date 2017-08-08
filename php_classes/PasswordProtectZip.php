8344e9d26eb67608f0feb30976bc3eab4b875404
Aaron C <aaron@acwpd.com>
2017-08-08 16:42:01 -0700

    Add Pasword-protected Zip

---
+++ php_classes/PasswordProtectZip.php
@@ -0,0 +1,31 @@
+<?php
+	
+/* Provides parsing tools for ACWPD Projects
+ * Feel free to use this in your projects! Just provide Attribution by keeping this block in place!
+ * 
+ * This is version 1.1
+ * 
+ * For the latest version, please visit: https://github.com/farfromunique/ACWPD_Tools
+ * 
+ * This code is copyright (C) 2016 Aaron Coquet / ACWPD
+ */ 
+
+function passwordProtectInZip(string $ZipFilename = 'output.zip', string $password,string ...$files) {
+	$zip = new ZipArchive;
+	$zip->open($ZipFilename, ZipArchive::CREATE);
+	$zip->setPassword($password);
+	foreach ($files as $file) {
+		$zip->addFile($file);
+		$zip->setEncryptionName($file, ZipArchive::EM_AES_256);
+	}
+	$zip->close();
+
+	///Then download the zipped file.
+	header('Content-Type: application/zip');
+	header('Content-disposition: attachment; filename='.$ZipFilename);
+	header('Content-Length: ' . filesize($ZipFilename));
+	readfile($ZipFilename);
+}
+
+
+?>

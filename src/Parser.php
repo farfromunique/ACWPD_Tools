838a88dd73a6fa4ce5c740d3fb38ec3756c67a41
Aaron Coquet <aaron@acwpd.com>
2018-06-16 20:14:34 -0700

    Cleaner variable name

--- src/Parser.php
+++ src/Parser.php
@@ -13,22 +13,23 @@ namespace ACWPD\Tools;
  */ 
  
 class Parser {
+
     /** @param string $str String containing a time in UTC surrounded by ":-:UTC:-:" on one side and ":-:" on the other.
      * @param mixed $TimeZoneOffset The difference from a given timezone.
      * @param string $timeFormat The PHP-style time format desired by the user.
      * @return string The string returned either has the time adjusted by the offset given, or (on error) the original text.
      */
 	public function fixUTC($str,$TimeZoneOffset,$timeFormat) {
-		$bits = explode(":-:",$str);
-		for ($i=0;$i<count($bits);$i++) {
-			if ($bits[$i] == "UTC") {
-				$tds = explode(' ',$bits[$i + 1]); // Format is (2013-08-14 05:00:00)
+		$pieces = explode(":-:",$str);
+		for ($i=0;$i<count($pieces);$i++) {
+			if ($pieces[$i] == "UTC") {
+				$tds = explode(' ',$pieces[$i + 1]); // Format is (2013-08-14 05:00:00)
 				$tds_date = explode('-',$tds[0]);
 				$tds_time = explode(':',$tds[1]);
 				$fixTime = date($timeFormat,mktime($tds_time[0],$tds_time[1],$tds_time[2],$tds_date[1],$tds_date[2],$tds_date[0]) + $TimeZoneOffset);
-				$bits[$i] = '';
-				$bits[$i+1] = $fixTime;
-				$output = implode($bits);
+				$pieces[$i] = '';
+				$pieces[$i+1] = $fixTime;
+				$output = implode($pieces);
 				return $output;
 			}
 		}
